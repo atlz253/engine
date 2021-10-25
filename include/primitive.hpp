75fc670c6b42eb64191d0f5bc368696eeb8b22e0
Fedor <f.alexeeff2010@yandex.ru>
2021-10-25 12:48:47 +0300

    primitives header

---
+++ include/primitive.hpp
@@ -0,0 +1,40 @@
+#include "scene.hpp"
+#include "type.hpp"
+
+namespace engn
+{
+    class Rectangle final : IScene
+    {
+    private:
+        Rect *rect;
+        Color *color;
+
+        void Init(void) override;
+        void Destroy(void) override;
+
+    public:
+        Rectangle(Rect &rect, Color &color); // TODO: standart parametrs
+
+        void Render(void) override;
+
+        ~Rectangle();
+    };
+
+    class Line final : IScene
+    {
+    private:
+        Point *start;
+        Point *end;
+        Color *color;
+
+        void Init(void) override;
+        void Destroy(void) override;
+
+    public:
+        Line(Point &start, Point &end);
+
+        void Render(void) override;
+
+        ~Line();
+    };
+}
