844bfde7fb16b96a81f541c0eec7fef3119da3b1
Fedor <f.alexeeff2010@yandex.ru>
2021-10-26 15:35:40 +0300

    exception test

---
+++ test/exception.cpp
@@ -0,0 +1,23 @@
+#include "exception.hpp"
+
+#include "handler.hpp"
+
+#include <iostream>
+
+using namespace engn;
+
+int main()
+{
+    try
+    {
+        GameHandler handler1, handler2;
+
+        std::cerr << "DoubleInitException not work!" << std::endl;
+    }
+    catch(const DoubleInitException& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
